8deefca5d95812103159002f5ca41ea52b9ade10
Satyakam <bb1190050@iitd.ac.in>
2020-05-15 13:40:32 +0530

    may15

---
+++ js/script2.js
@@ -0,0 +1,26 @@
+alert("Use Round Button to play/pause the audio 🔘\nUse Slider to control volume 🎚️" );
+		var state=0;
+		$(window).load(function(){
+             $(".loader").hide();
+             $(".player").show();
+        });
+		$(document).ready(function(){
+			$(".power").click(function(){
+  				$(".record").toggleClass('on');
+  				$(".stick").toggleClass('play');
+  				if(state==0){
+  					setTimeout(function(){
+  					 $(".mysong")[0].play();
+  					},1000);
+  					state=1;
+  				}
+  				else{
+  					$(".mysong")[0].pause();
+  					state=0;
+  				}
+  				$(".slider").change(function(){
+  					$(".mysong")[0].volume=this.value;
+  				});
+  			});
+  			
+		});
