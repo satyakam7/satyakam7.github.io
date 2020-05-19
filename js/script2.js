918bb08e6f86169c7c525c6ccc102b20c99fea78
Satyakam Singhal <satyakam068@gmail.com>
2020-05-19 12:05:31 +0530

    m

--- js/script2.js
+++ js/script2.js
@@ -1,15 +1,15 @@
 // Some random colors
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-const numBalls = 500;
+const numBalls = 700;
 const balls = [];
 
 for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
+  let ball = document.createElement("section");
   ball.classList.add("ball");
   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}%`;
-  ball.style.top = `${Math.floor(Math.random() * 6000)}px`;
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 5500)}px`;
   ball.style.transform = `scale(${Math.random()})`;
   ball.style.width = `${Math.random()}em`;
   ball.style.height = ball.style.width;
@@ -27,8 +27,7 @@ balls.forEach((el, i, ra) => {
 
   let anim = el.animate(
     [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    
     ],
     {
       duration: (Math.random() + 1) * 2000, // random duration
