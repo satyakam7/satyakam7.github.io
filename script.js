c7dc881ef11032cb4aca364e08f2e74e060c5904
Satyakam Singhal <satyakam068@gmail.com>
2020-06-18 18:59:07 +0530

    qw

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var i = 0;
 
-var txt = "Hey! Thanks for visiting my blog This is sort of my daily life journal which I write at the end of the end which is usually 4 AM for me Sorry i haven't updated it from quite a long time cause i have been a little busy lately. Have a nice day :) //Loading.................................. ";
+var txt = "Hey! This is my daily life journal where I kind off document my work at the EOD This might look a lil formal and boring but still if you want to view it I have left some music inside. Hope you have a great time. :) //Loading.................................. ";
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementsByClassName('js-typewrite')[0].innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter, 65);
+    setTimeout(typeWriter, 45);
   }
 }
 
