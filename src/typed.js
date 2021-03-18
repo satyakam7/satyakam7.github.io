987e8f301ab4661b447f81b4e0da38d91e990c66
Satyakam Singhal <satyakam068@gmail.com>
2021-03-18 05:34:30 +0530

    lmao

---
+++ src/typed.js
@@ -0,0 +1,13 @@
+new Typed('.js-about-info', {
+  strings: [
+    'Muser',
+    'Dysfunctional Programmer',
+    'lifelong learner',
+    'problem solver',
+    'procrastinator',
+    'overthinker',
+  ],
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 1200
+});
